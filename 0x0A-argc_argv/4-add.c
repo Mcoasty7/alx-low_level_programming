e7db4a254d294eee2dda4749568aeb7af74616a0
Mcoasty7 <michael.wanyama6@gmail.com>
2022-10-01 09:53:59 -0700

    Task 5

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -4,7 +4,7 @@
 /**
  * main - this is a function to print its name
  * @argc: argc parameter
- * @argv: an array of a command listed
+g* @argv: an array of a command listed
  * Return: 0 for success
  */
 
@@ -12,19 +12,19 @@ int main(int argc, char *argv[])
 {
 	int result = 0, num, i, j, k;
 
-	if (argc == 1)
-		printf("0\n");
-
-	for (i = 0; i < argc; i++)
+	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > '9' || argv[i][j] < '0')
 		{
 			printf("%s\n", "Error");
 			return (1);
 		}
+		}
 	}
 
-	for (k = 0; k < argc; k++)
+	for (k = 1; k < argc; k++)
 	{
 		num = atoi(argv[k]);
 		result += num;
